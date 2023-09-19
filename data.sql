32e1d9e2f63cba09db6588f498dcbdbb9bd658ad
diarisdiakite <diarisdiakite@gmail.com>
2023-09-19 07:49:39 +0200

    Add data in species and owners tables.

--- data.sql
+++ data.sql
@@ -21,3 +21,18 @@ VALUES
   ('Boarmon', '2005-06-07', 7, true, 20.4, null),
   ('Blossom', '1998-10-13', 3, true, 17, null),
   ('Ditto', '2022-05-14', 4, true, 22, null);
+
+
+INSERT INTO owners(full_name, age) 
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+
+INSERT INTO species (name) Values
+('Pokemon'),
+('Digimon');
